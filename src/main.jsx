f9746ad277081e190b9da13853028b784c9a7a1c
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-08-02 00:41:09 +0200

    Home page fini - niveau CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./assets/pages/Homepage";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
