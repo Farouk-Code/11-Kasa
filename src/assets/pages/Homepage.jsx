755a8dfffc8831163c37203b227dee76f8863353
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-08-02 00:21:14 +0200

    Home page fini - niveau react

---
+++ src/assets/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Layout from "../components/Layout";
+
+const Homepage = () => {
+  return (
+    <>
+      <Layout>
+        <Header />
+      </Layout>
+    </>
+  );
+};
+
+export default Homepage;
