f0f8f4aa241e4bed7d8e58630a9b23e3572d8cc3
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-08-10 00:34:42 +0200

    composants error fini

--- src/assets/components/error.jsx
+++ src/assets/components/error.jsx
@@ -1,3 +1,16 @@
-const Error = () => {};
+/* eslint-disable react/no-unescaped-entities */
+import { Link } from "react-router-dom";
+
+const Error = () => {
+  return (
+    <div className="error-page">
+      <h1>404</h1>
+      <p className="error-message">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="error-redirect">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
+};
 
 export default Error;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./assets/pages/Homepage";
 import AboutPage from "./assets/pages/Aboutpage";
+import ErrorPage from "./assets/pages/Errorpage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Homepage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/about",
