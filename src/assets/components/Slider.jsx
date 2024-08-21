8e1d2d61db62a4a52d8807347168bb94675f0663
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-08-21 22:12:57 +0200

    update

--- src/assets/components/Slider.jsx
+++ src/assets/components/Slider.jsx
@@ -21,6 +21,7 @@ const Slider = ({ images }) => {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const shouldRenderButtons = images.length > 1;
